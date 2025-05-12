8ce55b6d076dba6fdc5f29cfe60fd88918af0049
RageJohan <johan.lopez@tecsup.edu.pe>
2025-05-12 14:40:58 -0500

    db v4

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,21 +1,21 @@
 // app/config/db.config.js
 export default {
-    HOST: process.env.DB_HOST || "dpg-d9h2fqq4456c738cc1o9-a.oregon-postgres.render.com",
-    USER: process.env.DB_USER || "root",
-    PASSWORD: process.env.DB_PASSWORD || "30Jrbtfpv7PJSVqVvhdd0dru07rqjI02",
-    DB: process.env.DB_NAME || "db_3vzp",
-    PORT: process.env.DB_PORT || 5432,
-    dialect: "postgres",
-    dialectOptions: {
-        ssl: {
-            require: true,          // Obliga conexión SSL
-            rejectUnauthorized: false // Necesario para Render
-        }
+  HOST: process.env.DB_HOST || "dpg-d9h2fqq4456c738cc1o9-a.oregon-postgres.render.com",
+  USER: process.env.DB_USER || "root",
+  PASSWORD: process.env.DB_PASSWORD || "30Jrbtfpv7PJSVqVvhdd0dru07rqjI02",
+  DB: process.env.DB_NAME || "db_3vzp",
+  PORT: process.env.DB_PORT || 5432,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-};
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+}
